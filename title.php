53256adbb4ce6b203b745af955f72e396f422bae
soso101 <109085716@qq.com>
2016-09-20 16:31:21 +0800

    style

--- title.php
+++ title.php
@@ -7,18 +7,38 @@
  */
 //获取登录信息
 $islogin = false;
+$user = 'aaa';
+/*
 if ($islogin)
 {
     echo "<a href=\"userinfo.php\" id=\"userinfo\" class=\"userinfo\">".$user."</a>";
 }
 else
 {
-    echo "<form class=\"login\" method=\"post\" action=\"logincheck.php\">";
     echo "<div id=\"userlogin\">";
+    echo "<form class=\"login\" method=\"post\" action=\"logincheck.php\">";
     echo "name:<input type=\"text\" name=\"username\">";
     echo "password:<input type=\"password\" name=\"password\">";
     echo "<input type=\"submit\" name=\"submit\" value=\"login\">";
     echo "<a href=\"register.php\">register</a>";
-    echo "</div>";
     echo "</form>";
+    echo "</div>";
 }
+*/
+?>
+
+<img src="img/MAMP-PRO-Logo.png">
+<?php if($islogin): ?>
+<a href="userinfo.php" id="userinfo" class="userinfo"><?php echo $user ?></a>
+<?php else: ?>
+<div id="userlogin">
+    <form class="login" method="post" action="logincheck.php">
+        name:<input type="text" name="username">
+        <br>
+        password:<input type="password" name="password">
+        <br>
+        <input type="submit" name="submit" value="login">
+        <a href="register.php">register</a>
+    </form>
+</div>
+<?php endif;?>
